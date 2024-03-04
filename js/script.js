d44cc64072b20b89c8539c04a9a8fb1d84467083
Francesco <francescopesoli63@gmail.com>
2024-03-04 19:47:05 +0100

    updated the course program

--- js/script.js
+++ js/script.js
@@ -9,3 +9,44 @@ window.addEventListener("load", function(){
 document.querySelector("#close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
 });
+
+const onSuccess = function(response) {
+    alert(" Email inviata con successo!");
+    console.log(response);
+};
+
+const onError = function(err) {
+    alert("Errore nell'invio dell'email, riprova più tardi");
+    console.error(err);
+};
+
+// TODO: replace the endpoint url with your own
+sendEmail("https://public.herotofu.com/v1/b37fbdc0-da56-11ee-8a16-2dace5ed8cab", {
+    example_user: "user@example.com",
+    example_data: new Date().toISOString(),
+}, onSuccess, onError);
+
+// The same code as in previous snippet...
+function sendEmail(endpointUrl, data, onSuccess, onError) {
+    $.ajax({
+        type: "POST",
+        url: endpointUrl,
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: onSuccess,
+        error: function(xhr, status) {
+            if (typeof this.statusCode[xhr.status] !== 'undefined') {
+                return false;
+            }
+
+            onError(err);
+        },
+        statusCode: {
+            // Endpoint thinks that it's likely a spam/bot request, you need to change "spam protection mode" to "never" in HeroTofu forms
+            422: function(response) {
+                alert("Cannot send request, are you robot?");
+            },
+        }
+    });
+}
